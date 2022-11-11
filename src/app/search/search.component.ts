9e999bb2ebf2338ad7aa96eac5525ab76ceca33a
Ankur Sharma <ankur.101045@gmail.com>
2022-11-11 19:27:32 +0530

    added duplicate check

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -26,13 +26,25 @@ export class SearchComponent implements OnInit {
           let url = trObj['url'];
           let trackData: any = await this.http.post(`https://tech-blogs-aggregator-api.vercel.app/mr`,{'url': url}).toPromise();
             obj['url'] = this._sanitizer.bypassSecurityTrustResourceUrl(trackData['url']);
-            this.trackList.push(obj);
+            if(!this.checkDuplicate(obj.name))
+              this.trackList.push(obj);
         }
       })
     }
     else if(this.searchText.length<3){
       this.trackList = [];
     }
+  }
+
+  checkDuplicate(trackName: string){
+    let found = false;
+    for(let track of this.trackList){
+        if(trackName == track.name){
+          found = true;
+          break;
+        }
+    }
+    return found;
   }
 
   showVideo(trackName: any){
