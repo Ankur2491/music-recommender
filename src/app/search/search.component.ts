0e2834b6d1d2808f6160ff38bb8a1d6ff50ca01f
Ankur Sharma <ankur.101045@gmail.com>
2022-11-11 23:13:36 +0530

    added unsubscribe for observable

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,43 +11,50 @@ export class SearchComponent implements OnInit {
 
   searchText = "";
   trackList: Array<any> = [];
-  selected='';
+  selected = '';
+  mainSub: any;
+  childSub: any;
   constructor(private http: HttpClient, private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
   search() {
-    if (this.searchText.length > 2) {
-      this.http.get(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${this.searchText}&api_key=7b9b811126f62cd662af3bd98bd57966&format=json`).subscribe(async (data: any)=>{
-        let tracks = data.results.trackmatches.track;
-        for(let trObj of tracks){
-          let obj: any = {'name': trObj.name, 'artist': trObj.artist};
-          let url = trObj['url'];
-          let trackData: any = await this.http.post(`https://tech-blogs-aggregator-api.vercel.app/mr`,{'url': url}).toPromise();
-            obj['url'] = this._sanitizer.bypassSecurityTrustResourceUrl(trackData['url']);
-            if(!this.checkDuplicate(obj.name))
-              this.trackList.push(obj);
-        }
-      })
+    this.trackList = [];
+    if(this.mainSub && this.childSub){
+    this.mainSub.unsubscribe;
+    this.childSub.unsubscribe;
     }
-    else if(this.searchText.length<3){
-      this.trackList = [];
+    if (this.searchText.length > 2) {
+      this.mainSub = this.http.get(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${this.searchText}&api_key=7b9b811126f62cd662af3bd98bd57966&format=json`).subscribe((data: any) => {
+        let tracks = data.results.trackmatches.track;
+        for (let trObj of tracks) {
+          let obj: any = { 'name': trObj.name, 'artist': trObj.artist };
+          let url = trObj['url'];
+          this.childSub = this.http.post(`https://tech-blogs-aggregator-api.vercel.app/mr`, { 'url': url })
+          .subscribe((trackData: any)=>{
+            obj['url'] = this._sanitizer.bypassSecurityTrustResourceUrl(trackData['url']);
+            if (!this.checkDuplicate(obj.name))
+              this.trackList.push(obj);
+          })
+        }
+      });
+
     }
   }
 
-  checkDuplicate(trackName: string){
+  checkDuplicate(trackName: string) {
     let found = false;
-    for(let track of this.trackList){
-        if(trackName == track.name){
-          found = true;
-          break;
-        }
+    for (let track of this.trackList) {
+      if (trackName == track.name) {
+        found = true;
+        break;
+      }
     }
     return found;
   }
 
-  showVideo(trackName: any){
+  showVideo(trackName: any) {
     this.selected = trackName;
   }
 
