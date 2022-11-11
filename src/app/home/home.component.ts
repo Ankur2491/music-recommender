67c3bf6f6302710769bdc836acd61277dbb6a034
Ankur Sharma <ankur.101045@gmail.com>
2022-11-11 17:30:08 +0530

    retained sort order

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,18 +16,19 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient, private safePipe: SafePipe, private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    this.http.get(`https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7b9b811126f62cd662af3bd98bd57966&format=json`).subscribe((data: any)=>{
+    this.http.get(`https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7b9b811126f62cd662af3bd98bd57966&format=json`).subscribe(async (data: any)=>{
       let tracks = data.tracks.track;
       for(let trObj of tracks){
         let obj: any = {'name': trObj.name, 'playcount': trObj.playcount, 'artist': trObj.artist.name};
         let url = trObj['url'];
-        this.http.post(`https://tech-blogs-aggregator-api.vercel.app/mr`,{'url': url}).subscribe((trackData: any)=>{
+        let trackData: any = await this.http.post(`https://tech-blogs-aggregator-api.vercel.app/mr`,{'url': url}).toPromise();
           obj['url'] = this._sanitizer.bypassSecurityTrustResourceUrl(trackData['url']);
           this.trackList.push(obj);
-          console.log(this.trackList);
-        })
       }
     })
+  }
+  sortTracks(){
+    this.trackList.sort((a,b)=>{return (Number(b['playcount']) - Number(a['playcount']))})
   }
   showVideo(trackName: any){
     this.selected = trackName;
